7258eccacb65ed867ca5beeb98481e9304c91f9b
aditi002 <aditi1chalisay@gmail.com>
2023-04-18 11:36:04 +0545

    books with json data

---
+++ data/books.js
@@ -0,0 +1,14 @@
+const books=[{
+    id:1,
+    title: "Js Fundamentals",
+    author: "Adu padhu Chalisay"
+},
+{
+    id:2,
+    title:"War and Peace",
+    author: "Leo Tostoy"
+}
+]
+
+
+module.exports = books
